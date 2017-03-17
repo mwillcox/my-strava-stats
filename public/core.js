64001cda0e820fc0b9979f89cd84f0016bf78194
Michelle Willcox <shellwillcox@gmail.com>
2017-03-17 10:06:33 -0700

    dynamically building activity maps

--- public/core.js
+++ public/core.js
@@ -1,64 +1,98 @@
-var stravaStats = angular.module('stravaStats', []);
+var stravaStats = angular.module('stravaStats', [])
+  .directive('onFinishRender',['$timeout', '$parse', function ($timeout, $parse) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            if (scope.$last === true) {
+                $timeout(function () {
+                    scope.$emit('ngRepeatFinished');
+                    if(!!attr.onFinishRender){
+                      $parse(attr.onFinishRender)(scope);
+                    }
+                });
+            }
+        }
+    }
+}]);
 
 function mainController($scope, $http) {
 
-  $http.get('/api/stats')
-    .success(function(data) {
-      $scope.stats = data[0];
-    })
-    .error(function(data) {
-      console.log('Error: ' + data);
-    });
+  $scope.newActivities= [];
 
-  $http.get('/api/strava/athelete')
+  $scope.onLoad = function(){
+    
+    $http.get('/api/strava/athelete')
     .success(function(data) {
       $scope.athelete = data;
     })
     .error(function(data) {
       console.log('Error: ' + data);
     });
+    
+    $http.get('/api/stats')
+      .success(function(data) {
+        $scope.stats = data[0];
+      })
+      .error(function(data) {
+        console.log('Error: ' + data);
+    });
 
     $http.get('/api/strava/activities')
       .success(function(data) {
         $scope.activity = data[0];
-    })
-    .error(function(data) {
-      console.log('Error: ' + data);
+        //populate dynamically
+        buildActivties(data.slice(5));
+      })
+      .error(function(data) {
+        console.log('Error: ' + data);
     });
-    
+
+  };
+
+  var buildActivties = function(data){
+    for (var i = 0; i < 5; i++){
+      var activity = {
+        name: data[i].name,
+        distance: getMiles(data[i].distance),
+        map: data[i].map.summary_polyline
+      };
+      $scope.newActivities.push(activity);
+      
+    }
+    console.log($scope.newActivities);
+  };
+
+  $scope.buildMaps = function(){
+    for (var i = 0; i < $scope.newActivities.length; i++){
+      var myLatLng = new google.maps.LatLng(37.773972, -122.431297);
+      var mapOptions = {
+        zoom: 12,
+        center: myLatLng,
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+      };
+
+      var route;
+       var map = new google.maps.Map(document.getElementById("map-" + i),
+        mapOptions);
+
+       // Construct the polygon
+      route = new google.maps.Polygon({
+        paths: google.maps.geometry.encoding.decodePath($scope.newActivities[i].map),
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0
+      });
+
+      route.setMap(map);
+      map.setCenter(route.getPath().getAt(Math.round(route.getPath().getLength() / 2)));
+    }
+  }
+
 
   function getMiles(i) {
      return i*0.000621371192;
-  }
-
-  $scope.onLoad = function(){
-
-    var myLatLng = new google.maps.LatLng(37.773972, -122.431297);
-    var mapOptions = {
-      zoom: 12,
-      center: myLatLng,
-      mapTypeId: google.maps.MapTypeId.TERRAIN
-    };
-
-    var bermudaTriangle;
-
-    var map = new google.maps.Map(document.getElementById('map'),
-      mapOptions);
-
-
-    // Construct the polygon
-    bermudaTriangle = new google.maps.Polygon({
-      paths: google.maps.geometry.encoding.decodePath("}{teFdocjViDv@O|AzRtuCpyAoQzCp]zCCz@~G`EEt@rHjKe@`G`q@P|U`FpQi@|EsBgEuDqB{C~Bo@`C`CdL{@~A{Bi@zBhGk@|EbAjFKnLnBrF`@rKo@tCnCrIkBnErD|GaCfJn@~PpAhGnC_@nB|DpHcCpGvAfH}Hg@`CsHlGa@~H`@fGdCbFeDdHfHxT[hEjBfJyDb@|@bSiHvAtE`@`BxKiBdGeCBZnFbEg@oAfGtD~SwA~HeBhBpHY`@lGbB?rAnG{M|AwNq@qD`Fob@jBeKjK}Cl@[gFdCbBsB}@HwG|AWFrGnD}DSlBm@KbA{CiDyAE~AM{B_HuFkDl@kCat@iMV}BlBcFcIZgCoAdCgCwDzBeTqD_RzAaIeBsGsGw@tCdGyCqF|A?NsMYgR{B{H{b@kMoFNcKcJaFn@gKsC}ScNnRAtAwBjJqB|@oKrD_G|FcDCmI_DcMOaJtJwB|AeGxIqCjBaJ|AUzCcG~Bm@fA`D~ABq@_KfIoB_PsiCcBeBiKnA_C_FeFw|@YuKfA["),
-      strokeColor: '#FF0000',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: '#FF0000',
-      fillOpacity: 0
-    });
-
-    bermudaTriangle.setMap(map);
-    map.setCenter(bermudaTriangle.getPath().getAt(Math.round(bermudaTriangle.getPath().getLength() / 2)));
-  
   }
 
 }
--- server.js
+++ server.js
@@ -6,16 +6,6 @@ var morgan = require('morgan');             // log requests to the console (expr
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
-// configuration =================
-//mongoose.connect('mongodb://test:test@ds043262.mlab.com:43262/mwnodetodo');     // connect to mongoDB database on modulus.io
-
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride());
-
 // routes ======================================================================
 require('./app/routes.js')(app);
 
@@ -35,9 +25,15 @@ mongoose.connect('mongodb://test:test@ds123410.mlab.com:23410/heroku_nbv000xn', 
     console.log("App now running on port", port);
   });
 });     
-// connect to mongoDB database on modulus.io
 
-// ACTIVITIES API ROUTES BELOW
+app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
   console.log("ERROR: " + reason);
